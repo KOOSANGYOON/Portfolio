cb48531a59e81ae9d0321941b78fd6d1c3fd43bf
KOOSANGYOON <nevergiveup936@naver.com>
2018-04-03 16:24:24 +0900

    initial commit

---
+++ web-portfolio/src/main/java/webportfolio/WebPortfolioApplication.java
@@ -0,0 +1,12 @@
+package webportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPortfolioApplication.class, args);
+	}
+}
