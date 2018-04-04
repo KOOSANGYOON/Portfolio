ac79806924def25bda588a63c8a2c05ea600daba
KOOSANGYOON <nevergiveup936@naver.com>
2018-04-04 12:23:43 +0900

    update

---
+++ web-portfolio/src/test/java/web/HomeAcceptanceTest.java
@@ -0,0 +1,14 @@
+package web;
+
+//import static org.junit.Assert.*;
+
+import org.junit.Test;
+//import org.springframework.http.ResponseEntity;
+
+public class HomeAcceptanceTest {
+
+	@Test
+	public void test() {
+	}
+
+}
